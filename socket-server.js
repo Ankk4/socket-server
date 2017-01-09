eaaa35bf051a04963dde6c6b4bbb6015b02dc766
Ankk4 <antero.juutinen@gmail.com>
2017-01-09 13:36:51 +0000

    small changes

--- socket-server.js
+++ socket-server.js
@@ -1,5 +1,5 @@
 var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ port: 8080 });
+var wss = new WebSocketServer({ port: 80 });
 
 wss.on('connection', function connection(ws) {
     console.log("A New connection!");
